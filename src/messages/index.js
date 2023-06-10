a0953c9e5bc986d8bee2b5f0be5d8ff55836dfe2
주형돈 <hdju@rntime.com>
2023-06-10 15:01:18 +0900

    i18n 적용

---
+++ src/messages/index.js
@@ -0,0 +1,8 @@
+import ko from './langs/ko'
+import en from './langs/en'
+const messages = {
+    ko,
+    en,
+}
+export default messages;
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const routes = [
-    { name: 'Home', path: '/', component: () => import('../pages/Home.vue') },
-    {name: 'About', path: '/about', component: () => import('../pages/About.vue') },
-    {name: 'Member', path: '/member', component: () => import('../pages/Member.vue') },
+    { name: 'home', path: '/', component: () => import('../pages/Home.vue') },
+    { name: 'about', path: '/about', component: () => import('../pages/About.vue') },
+    { name: 'member', path: '/member', component: () => import('../pages/Member.vue') },
 ]
 
 
