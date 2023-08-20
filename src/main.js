513e1b4073dfc1004bda099cc7a171febeb97b4b
주형돈 <hdju@rntime.com>
2023-08-20 14:55:33 +0900

    vuetify 3 세팅

--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ import messages from './messages'
 import './style.css'
 import App from './App.vue'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
@@ -24,4 +35,5 @@ createApp(App)
     .use(router)
     .use(pinia)
     .use(i18n)
+    .use(vuetify)
     .mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,15 @@
 <script setup>
+const items = [
+        { title: 'Item #1', value: 1, },
+        { title: 'Item #2', value: 2, },
+        { title: 'Item #3', value: 3, }, ]
 </script>
 
 <template>
- <h1>Home</h1> 
+  <h1>Home</h1>
+  <v-card class="mx-auto" max-width="300">
+    <v-list :items="items"></v-list>
+  </v-card>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
