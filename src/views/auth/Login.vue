ff99865f5302bcad39798687d6c83ae82f2e3297
주형돈 <hdju@rntime.com>
2023-10-15 10:08:42 +0900

    로그인 유효성 검사

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,27 +1,47 @@
 <script lang="ts" setup>
 import { useAuthStore, LoginUser } from '@/store/AuthService';
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 import { useI18n } from 'vue-i18n';
 
-const authStore = useAuthStore()
+const authService = useAuthStore()
 const { t } = useI18n()
+
 const user: LoginUser = reactive({
     email: 'matthew@ohhoonim.dev',
     password: '1234',
 })
-function login(): void {
-    authStore.login(user)
- }
+const loading = ref(false)
+async function login(event: any) {
+    loading.value = true
+    const results = await event
+    const errors = await results.errors
+    if (errors?.length === 0) {
+        authService.login(user)
+    }
+    loading.value = false
+}
+const emailRules = [
+    (value: string) => {
+        if (value) return true
+        return 'E-mail is requred.'
+    },
+    (value: string) => {
+        if (/.+@.+\..+/.test(value)) return true
+        return 'E-mail must be valid.'
+    },
+]
 </script>
 <template>
-    <div class="d-flex align-center justify-center" style="height: 100vh">
+    <div class="d-flex align-center justify-center" style="height: 90vh">
         <v-sheet width="400" class="mx-auto">
             <h2>{{ t('hello-the-factory') }}</h2>
-            <v-form fast-fail @submit.prevent="login">
-                <v-text-field variant="outlined" v-model="user.email" :label="t('username')"></v-text-field>
-                <v-text-field type="password" variant="outlined" v-model="user.password" :label="t('password')"></v-text-field>
+            <v-form validate-on="submit lazy" @submit.prevent="login">
+                <v-text-field v-model="user.email" :label="t('username')" :rules="emailRules" required
+                    variant="outlined"></v-text-field>
+                <v-text-field v-model="user.password" type="password" :label="t('password')"
+                    :rules="[(v: any) => !!v || t('password') + 'is required']" required variant="outlined"></v-text-field>
                 <a href="#" class="text-body-2 font-weight-regular">{{ t('forgot-password') }}</a>
-                <v-btn type="submit" @click.prevent="login" color="primary" block class="mt-2">{{ t('sign-in') }}</v-btn>
+                <v-btn type="submit" block :loading="loading" color="primary" class="mt-2">{{ t('sign-in') }}</v-btn>
             </v-form>
             <div class="mt-2">
                 <p class="text-body-2">{{ t('dont-have-an-account') }} <a href="#">{{ t('sign-up') }}</a></p>
@@ -29,4 +49,3 @@ function login(): void {
         </v-sheet>
     </div>
 </template>
-@/store/authService@/store/AuthService
