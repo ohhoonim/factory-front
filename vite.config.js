39a4c3625188ea3979334658e92ffa69efb836d4
주형돈 <hdju@rntime.com>
2023-11-02 08:11:10 +0900

    proxy 설정변경

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,12 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
     }
   },
   
